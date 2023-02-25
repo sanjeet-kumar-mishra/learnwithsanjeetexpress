37d290e74ff391c54b19ca04a86ab9cb29ae733f
Sanjeet Kumar Mishra <sanjitkumarmishra4@gmail.com>
2023-02-25 23:28:28 +0530

    manpowercalc route

--- index.js
+++ index.js
@@ -2,37 +2,18 @@ const express = require('express')
 const app = express()
 const port = 8000
 const path = require('path')
-const client = require('./DB/db')
+const manpowercalc = require('./Routers/manpowercalc')
 
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname, "../learnwithsanjeet/build")))
 
-app.get('/', (req, res) =>{
+app.get('*', (req, res) =>{
     res.sendFile(path.join(__dirname, "../learnwithsanjeet/build", "index.html"))
 })
 
-app.get('/services/employeedetails', (req, res) =>{
-    res.sendFile(path.join(__dirname, "../learnwithsanjeet/build", "index.html"))
-})
-
-app.post('/services/employeedetails', async (req, res) =>{
-    async function result(){
-        try {
-            await client.connect()
-            const db = client.db("EmployeeData")
-            const collection = db.collection("ActiveEmployees")
-            const aresult = await collection.findOne({"CARDNO": parseInt(req.body.searchemployee)})
-            res.setHeader('Content-Type', 'application/json')
-            res.send(aresult)
-        } catch (error) {
-            console.log(error)
-        } finally {
-            await client.close()
-        }
-    }
-    result()
-})
+//Routers Section
+app.use('/services/manpowercalc', manpowercalc)
 
 
 app.listen(port, () =>{
